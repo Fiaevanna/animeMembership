84fb13f2716bd355bd078d2e4d2194abb91a9bba
fiaevanna <fiaevanna@gmail.com>
2023-05-21 15:02:48 +0200

    updated to inputfild and did endpoint

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-function deleteMember() {
-    const id = window.location.pathname.split("/member/")[1];
-    console.log(id)
+async function deleteMember() {
+  const id = window.location.pathname.split("/member/")[1];
+  await fetch(`/member/${id}`, { method: "DELETE" });
+  window.location.replace("/members");
 }
-
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import { MongoClient, ObjectId } from "mongodb";
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
+import { MongoClient, ObjectId } from "mongodb";
 
 const port = 3001;
 const app = express();
@@ -12,8 +12,8 @@ app.use(express.urlencoded());
 
 //app.use(expressEjsLayouts)
 
-//här gör jag en public map som gör att jag har tillgång till css filer och bilder 
-app.use(express.static('public'))
+//här gör jag en public map som gör att jag har tillgång till css filer och bilder
+app.use(express.static("public"));
 
 // här lyssnar jag på anslutningar (startar express serven)
 app.listen(port, () => console.log(`Listening on ${port}`));
@@ -22,45 +22,81 @@ app.listen(port, () => console.log(`Listening on ${port}`));
 const client = new MongoClient("mongodb://127.0.0.1:27017");
 await client.connect();
 
-
 //här sparar jag databasen i en variabel som jag vill annvända.
 const db = client.db("animeClub");
 const membersCollection = db.collection("members");
 
-
-
 /* Tar endpointen som jag använder i webbläsaren och kopplar till en templet som ska skickas till webbläsaren */
 
 app.get("/", async (req, res) => {
-    res.render("home", { })
-})
-
-// Här hämtar jag alla medlemar från min collection i db och omvandlar dem till en array och ger dem till rätt templet.
-app.get("/members", async(req, res) => {
-    const members = await membersCollection.find({}).toArray();
-    res.render("membersList", { members })
-})
-
-
-
-app.get("/member/:id", async (req, res) => {
-    const member = await membersCollection.findOne({ _id: new ObjectId(req.params.id) });
-    const date = member.date ? new Date(member.date) : null;
-    const formattedDate = date ? `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}` : null;
-
-    res.render("memberDetail", {
-        id: member._id,
-        name: member.name,
-        email: member.email,
-        phone: member.phone,
-        date: formattedDate,
-        favoriteAnime: member.favoriteAnime
-    });
+  res.render("home", {});
 });
 
+// Här hämtar jag alla medlemar från min collection i db och omvandlar dem till en array och ger dem till rätt templet.
+app.get("/members", async (req, res) => {
+  const members = await membersCollection.find({}).toArray();
+  res.render("membersList", { members });
+});
 
+app.get("/member/:id", async (req, res) => {
+  const member = await membersCollection.findOne({
+    _id: new ObjectId(req.params.id),
+  });
+
+  if (member === null) {
+    res.redirect("/members");
+  }
+
+  const date = member.date ? new Date(member.date) : null;
+  const formattedDate = date
+    ? `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+    : null;
+
+  res.render("memberDetail", {
+    id: member._id,
+    name: member.name,
+    email: member.email,
+    phone: member.phone,
+    date: formattedDate,
+    favoriteAnime: member.favoriteAnime,
+  });
+});
 
 app.get("/members/create", (req, res) => {
-    res.render("createMember", { })
+  res.render("createMember", {});
+});
+
+app.post("/members/create", async (req, res) => {
+  await membersCollection.insertOne(req.body);
+  res.redirect("/members");
+});
+
+app.delete("/member/:id", async (req, res) => {
+  await membersCollection.deleteOne({ _id: new ObjectId(req.params.id) });
+  res.status(200).send();
+});
+
+/* 
+app.post("/update-member/:id", async (req,res) => {
+    await membersCollection.updateOne({ _id: new ObjectId(req.params.id)}, {... req.body})
+    res.redirect('/members')
 })
 
+  */
+
+app.post("/update-member/:id", async (req, res) => {
+  try {
+    const memberId = req.params.id;
+    const updatedMemberData = req.body;
+
+    await membersCollection.updateOne(
+      { _id: new ObjectId(memberId) },
+      { $set: updatedMemberData }
+    );
+
+    res.redirect("/members");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/members");
+  }
+});
